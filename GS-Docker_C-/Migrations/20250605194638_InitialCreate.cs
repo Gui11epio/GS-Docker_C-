35577b8173e9e0efbf1ddc8e0b380973b6907d57
gui11epio <guilherme.laiber@gmail.com>
2025-06-05 16:49:22 -0300

    Migrations criado

---
+++ GS-Docker_C-/Migrations/20250605194638_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace GS_Docker_C_.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TB_USUARIO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_USUARIO", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TB_ENDERECO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Bairro = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_ENDERECO", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TB_ENDERECO_TB_USUARIO_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "TB_USUARIO",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TB_ALERTAS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    DataHora = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    NivelRisco = table.Column<int>(type: "int", nullable: false),
+                    Probabilidade = table.Column<int>(type: "int", nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    EnderecoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_ALERTAS", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TB_ALERTAS_TB_ENDERECO_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "TB_ENDERECO",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TB_ALERTAS_TB_USUARIO_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "TB_USUARIO",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_ALERTAS_EnderecoId",
+                table: "TB_ALERTAS",
+                column: "EnderecoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_ALERTAS_UsuarioId",
+                table: "TB_ALERTAS",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_ENDERECO_UsuarioId",
+                table: "TB_ENDERECO",
+                column: "UsuarioId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_USUARIO_Email",
+                table: "TB_USUARIO",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_ALERTAS");
+
+            migrationBuilder.DropTable(
+                name: "TB_ENDERECO");
+
+            migrationBuilder.DropTable(
+                name: "TB_USUARIO");
+        }
+    }
+}
